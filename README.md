6a7596e28a09133e3aa433c2b36c8c5e53a5191d
tpltnt <1172976+tpltnt@users.noreply.github.com>
2018-07-10 00:26:05 +0200

    implemented three stage model

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # apt-track
 Track the status of package installations via apt. This can be used to generate (implicit) documentation of installation/patching processes.
+A package is tracked through three stages: 'preparing', 'configuring', 'installed'.
 
 # references
 * [Debian wiki: Advanced Package Tool](https://wiki.debian.org/Apt)
--- apt-tracker.py
+++ apt-tracker.py
@@ -3,9 +3,27 @@
 with open("/var/log/dpkg.log") as infile:
     for line in infile:
         chunks = line.split(' ')
+        data = {}
+
+        # extracting data for each stage
         if 'install' == chunks[2]:
-            date = chunks[0]
-            time = chunks[1]
-            package = chunks[3].split(':')[0]  # discard architecture
-            version = chunks[5][:-1]
-            print(package, version)
+            data['state'] = 'preparing'
+            data['date'] = chunks[0]
+            data['time'] = chunks[1]
+            data['package'] = chunks[3].split(':')[0]  # discard architecture
+            data['version'] = chunks[5][:-1]
+        if 'configure' == chunks[2]:
+            data['state'] = 'configuring'
+            data['date'] = chunks[0]
+            data['time'] = chunks[1]
+            data['package'] = chunks[3].split(':')[0]  # discard architecture
+            data['version'] = chunks[4]
+        if 'status' == chunks[2] and 'installed' == chunks[3]:
+            data['state'] = 'installed'
+            data['date'] = chunks[0]
+            data['time'] = chunks[1]
+            data['package'] = chunks[4].split(':')[0]  # discard architecture
+            data['version'] = chunks[5]
+
+        if data:
+            print(data)
