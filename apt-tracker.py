c8658437b21aa0972d11e7e76f469afd73ea735d
tpltnt <1172976+tpltnt@users.noreply.github.com>
2018-07-15 12:14:38 +0200

    PEP 8 fixes

--- apt-tracker.py
+++ apt-tracker.py
@@ -10,19 +10,19 @@ with open("/var/log/dpkg.log") as infile:
         data = {}
 
         # extracting data for each stage
-        if 'install' == chunks[2]:
+        if chunks[2] == 'install':
             data['state'] = 'preparing'
             data['date'] = chunks[0]
             data['time'] = chunks[1]
             data['package'] = chunks[3].split(':')[0]  # discard architecture
             data['version'] = chunks[5][:-1]
-        if 'configure' == chunks[2]:
+        if chunks[2] == 'configure':
             data['state'] = 'configuring'
             data['date'] = chunks[0]
             data['time'] = chunks[1]
             data['package'] = chunks[3].split(':')[0]  # discard architecture
             data['version'] = chunks[4].strip()
-        if 'status' == chunks[2] and 'installed' == chunks[3]:
+        if chunks[2] == 'status' and chunks[3] == 'installed':
             data['state'] = 'installed'
             data['date'] = chunks[0]
             data['time'] = chunks[1]
